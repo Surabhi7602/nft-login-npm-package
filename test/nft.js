eaa2cf67956c808a330f0bb730745e548d7a862a
Surabhi7602 <sshetty72@gatech.edu>
2022-11-15 09:55:11 -0500

    update test

--- test/nft.js
+++ test/nft.js
@@ -1,10 +1,4 @@
-// const chec = require("nft-login-npm-package");
-// checkNFT(
-//   "88484291872826645549206056394261872118284797679821832582028695917951321112826",
-//   "0x4fBe07b5c6973d69ED80729e33f47b7Eb3999744"
-// ).then((res) => console.log(res));
-
-const { verifyNFT } = require("nft-login-npm-package");
+const { verifyNFT } = require("nft-verify");
 verifyNFT(
   "88484291872826645549206056394261872118284797679821832582028695917951321112826",
   "0x4fBe07b5c6973d69ED80729e33f47b7Eb3999744"
